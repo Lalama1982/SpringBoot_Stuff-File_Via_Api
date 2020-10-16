4927287490d4ee512847089ef5b863c932d9abf7
Lalama1982 <lalamaesp@gmail.com>
2020-10-16 23:27:16 +1100

    WAR implementation for Tomcat

--- src/main/java/com/vscode/maven/springboot/filesviaapi/FilesviaapiApplication.java
+++ src/main/java/com/vscode/maven/springboot/filesviaapi/FilesviaapiApplication.java
@@ -2,12 +2,19 @@ package com.vscode.maven.springboot.filesviaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FilesviaapiApplication {
+public class FilesviaapiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilesviaapiApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(FilesviaapiApplication.class);
 	}
 
 }
